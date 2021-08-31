74f6701145643c1469ee1743152ef364ef0152ae
searinox0902 <54327789+searinox0902@users.noreply.github.com>
2021-08-31 12:25:57 -0500

    Add 14-Literal

---
+++ dev/14-Literal/js/main.js
@@ -0,0 +1,5 @@
+let nombre  = new String('Edwar el Extraño'); //Usando la Clase 'String' 
+let name = "string";                          //Literal
+
+let jugando = new Boolean(true);
+let jugando = true;
