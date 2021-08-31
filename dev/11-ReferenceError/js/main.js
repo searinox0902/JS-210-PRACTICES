4dedf651eaa42276836d2d3308b8fad2b642fcbd
searinox0902 <54327789+searinox0902@users.noreply.github.com>
2021-08-31 11:44:15 -0500

    Ejercicio Resuelto!

---
+++ dev/11-ReferenceError/js/main.js
@@ -0,0 +1,14 @@
+// Ejercicio 11 :  Resolver Problemas de Referencia de variable
+
+// =============================================================
+
+
+// var mensaje = "Variable global";
+
+function mostrarSaludo() {
+    var mensaje = "Hola soy un mensaje";
+
+    console.log(mensaje);
+}
+
+console.log(mensaje);
